4cbb501345391ed78efc57e38b1566f56e921813
Gorurai54 <goru78.nex@gmail.com>
2026-03-29 20:29:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const cors = require("cors");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-/* ========================= */
-/* CONFIG */
-/* ========================= */
-
-const SMTP_EMAIL = "goru78.nex@gmail.com";
-const SMTP_PASSWORD = "mjtlqbdyrxsakuhn";
-
-const APP_NAME = "Appnetick";
-
-/* ========================= */
-/* ROOT */
-/* ========================= */
-
-app.get("/", (req, res) => {
-res.send("SMTP Server Running");
-});
-
-/* ========================= */
-/* SEND OTP */
-/* ========================= */
-
 app.get("/send-otp", async (req, res) => {
 
 const email = req.query.email;
@@ -40,43 +9,40 @@ msg:"Email missing"
 });
 }
 
-console.log("Request received");
-
 const otp = Math.floor(100000 + Math.random() * 900000);
 
 try {
 
 const transporter = nodemailer.createTransport({
-service: "gmail",
+
+host: "smtp.gmail.com",
+port: 465,
+secure: true,
+
 auth: {
 user: SMTP_EMAIL,
 pass: SMTP_PASSWORD
 },
-connectionTimeout: 10000,
-greetingTimeout: 10000,
-socketTimeout: 10000
-});
 
-console.log("Sending email...");
+tls: {
+rejectUnauthorized: false
+},
+
+connectionTimeout: 20000,
+greetingTimeout: 20000,
+socketTimeout: 20000
+
+});
 
 await transporter.sendMail({
 
 from: `${APP_NAME} <${SMTP_EMAIL}>`,
 to: email,
-subject: "Appnetick OTP Verification",
+subject: "OTP Verification",
 
-html: `
-<div style="font-family:Arial;padding:20px">
-<h2>${APP_NAME}</h2>
-<p>Your OTP is:</p>
-<h1 style="font-size:40px;color:#4A90E2">${otp}</h1>
-<p>This OTP valid for 5 minutes</p>
-</div>
-`
+html: `<h1>${otp}</h1>`
 
 });
-
-console.log("Email sent");
 
 res.json({
 status:"success",
@@ -85,8 +51,6 @@ otp: otp
 
 } catch (e){
 
-console.log("Error:", e);
-
 res.json({
 status:"error",
 msg: e.toString()
@@ -94,14 +58,4 @@ msg: e.toString()
 
 }
 
-});
-
-/* ========================= */
-/* SERVER START */
-/* ========================= */
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-console.log("Server Running on port " + PORT);
 });
