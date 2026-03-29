300ab33411bcbcf7e2c63d51793d285e4ae8a255
Gorurai54 <goru78.nex@gmail.com>
2026-03-29 20:49:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* ========================= */
+/* CONFIG */
+/* ========================= */
+
+const SMTP_EMAIL = "9f2462001@smtp-brevo.com";
+const SMTP_PASSWORD = "s1cSLOD0xHz7GAF2";
+
+const APP_NAME = "Appnetick";
+
+/* ========================= */
+/* ROOT */
+/* ========================= */
+
+app.get("/", (req, res) => {
+res.send("SMTP Server Running");
+});
+
+/* ========================= */
+/* SEND OTP */
+/* ========================= */
+
 app.get("/send-otp", async (req, res) => {
 
 const email = req.query.email;
@@ -15,32 +46,31 @@ try {
 
 const transporter = nodemailer.createTransport({
 
-host: "smtp.gmail.com",
-port: 465,
-secure: true,
+host: "smtp-relay.brevo.com",
+port: 587,
+secure: false,
 
 auth: {
 user: SMTP_EMAIL,
 pass: SMTP_PASSWORD
-},
-
-tls: {
-rejectUnauthorized: false
-},
-
-connectionTimeout: 20000,
-greetingTimeout: 20000,
-socketTimeout: 20000
+}
 
 });
 
 await transporter.sendMail({
 
-from: `${APP_NAME} <${SMTP_EMAIL}>`,
+from: `"${APP_NAME}" <${SMTP_EMAIL}>`,
 to: email,
-subject: "OTP Verification",
+subject: "Appnetick OTP Verification",
 
-html: `<h1>${otp}</h1>`
+html: `
+<div style="font-family:Arial;padding:20px">
+<h2>${APP_NAME}</h2>
+<p>Your OTP is:</p>
+<h1 style="font-size:40px;color:#4A90E2">${otp}</h1>
+<p>This OTP valid for 5 minutes</p>
+</div>
+`
 
 });
 
@@ -58,4 +88,10 @@ msg: e.toString()
 
 }
 
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+console.log("Server Running");
 });
