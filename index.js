bf60ceb14c4c0b08c68eb3c4f1a23487b9b34296
Gorurai54 <goru78.nex@gmail.com>
2026-03-29 20:08:25 +0530

    Add root and test routes to the Express app

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 
 const app = express();
 
+// Root route
 app.get("/", (req, res) => {
   res.send("SMTP Server Running");
 });
 
+// Test route
 app.get("/send-otp", (req, res) => {
   res.send("OTP Route Working");
 });
 
+// Important for Render
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
