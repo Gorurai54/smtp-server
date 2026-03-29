bee07a17f893d5b8882eb54dfdff1d7acf4db584
Gorurai54 <goru78.nex@gmail.com>
2026-03-29 19:58:14 +0530

    Update server response messages

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server Running OK");
+res.send("Server Running");
 });
 
 app.get("/send-otp", (req, res) => {
-  res.send("OTP Route Working");
+res.send("OTP Working");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server started");
+console.log("Server running");
 });
