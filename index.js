1c6c76049063b7cc46b2777871855caaf9fc2046
Gorurai54 <goru78.nex@gmail.com>
2026-03-29 20:00:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-res.send("Server Running");
+  res.send("SMTP Server Running");
 });
 
 app.get("/send-otp", (req, res) => {
-res.send("OTP Working");
+  res.send("OTP Route Working");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-console.log("Server running");
+  console.log("Server running on port " + PORT);
 });
